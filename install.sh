50d1a12d3c34dcbdbaae121caf75bd00e08a6bf9
Ethan MacDonald <ethanmacdonald04@gmail.com>
2018-01-20 18:33:40 -0400

    found a solution for the odd grub-mkrescue issue

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
-echo Installing Dependencies...
-sudo apt install bchunk
+echo Installing Dependencies... 
 sudo apt install gcc
+#this fixes the weird grub-mkrescue fail issue
+sudo apt install grub-pc-bin
 echo Checking cross-compiler
 
 if [ ! -d cross-compiler ]; then
     if [ -d i686-elf-4.9.1-Linux-x86_64 ]; then
         mv i686-elf-4.9.1-Linux-x86_64 cross-compiler
     else
-        echo No cross-compiler found. Download from http://newos.org/toolchains/i686-elf-4.9.1-Linux-x86_64.tar.xz.
+        echo No cross-compiler found. Download from http://newos.org/toolchains/i686-elf-4.9.1-Linux-x86_64.tar.xz and rerun this script.
         exit 1
     fi
 fi
